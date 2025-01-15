08f89a6476e63ec5b85032888e9e5599b20f1a70
leoCappHarPar <leo.cappello@harvardpartners.com>
2025-01-15 09:51:59 -0500

    Updated globalAdmin script, active users only, mathcing upn and emails

--- getGlobalAdmin.ps1
+++ getGlobalAdmin.ps1
@@ -1,4 +1,9 @@
-﻿# Connect to Microsoft Graph
+﻿# Install Microsoft Graph module if not installed
+if (-not (Get-Module -ListAvailable -Name Microsoft.Graph)) {
+    #Install-Module -Name Microsoft.Graph -Scope CurrentUser -Force
+}
+
+# Connect to Microsoft Graph
 Connect-MgGraph -Scopes "RoleManagement.Read.Directory"
 
 # Define the Global Administrator Role ID
@@ -6,14 +11,19 @@ $globalAdminRoleId = "62e90394-69f5-4237-9190-012177145e10"
 
 # Get all members assigned to the Global Administrator role
 $globalAdmins = Get-MgRoleManagementDirectoryRoleAssignment -Filter "RoleDefinitionId eq '$globalAdminRoleId'" | ForEach-Object {
-    Get-MgUser -UserId $_.PrincipalId
+    Get-MgUser -UserId $_.PrincipalId -Property DisplayName, UserPrincipalName, Mail, AccountEnabled
+}
+
+# Filter for active users and ensure Mail and UserPrincipalName match
+$filteredAdmins = $globalAdmins | Where-Object {
+    $_.AccountEnabled -eq $true -and $_.Mail -eq $_.UserPrincipalName
 }
 
 # Define the output file path
-$outputFile = "C:\Reports\Global_Admin_Report.csv"
+$outputFile = "C:\Reports\Active_Global_Admin_Report.csv"
 
 # Select relevant properties
-$adminReport = $globalAdmins | Select-Object DisplayName, UserPrincipalName, Mail
+$adminReport = $filteredAdmins | Select-Object DisplayName, UserPrincipalName, Mail
 
 # Export the data to a CSV file
 $adminReport | Export-Csv -Path $outputFile -NoTypeInformation -Encoding UTF8
